c9402da799e8d37ff2b07a245d07112f5c6e099c
Ehsan <ehsan.bkh3@gmail.com>
2021-05-05 23:09:30 +0430

    Init

--- nestech/src/main/java/ir/sitecoder/nestech/Nestech.java
+++ nestech/src/main/java/ir/sitecoder/nestech/Nestech.java
@@ -5,11 +5,24 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Looper;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -18,18 +31,15 @@ import static org.json.JSONObject.wrap;
 
 public class Nestech {
     private static Socket mSocket;
-    private static SharedPreferences preferences;
-    private static boolean initConnection=false;
-    private static boolean loginConnection=false;
-    private static boolean registerConnection=false;
-    static {
-        try {
-            mSocket = IO.socket("http://ne20.ir/");
-            mSocket.connect();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+    private static SharedPreferences preferences=null;
+    public static RequestQueue requestQueue;
+    private Nestech(Context context)
+    {
+        requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        preferences  = PreferenceManager.getDefaultSharedPreferences(context);
+        //other stuf if you need
     }
+
     AsyncCallback callback = new AsyncCallback() {
         @Override
         public void handleResponse(NestechUser response) {
@@ -38,143 +48,166 @@ public class Nestech {
         public void handleFault(NestechFault fault) {
         }
     };
-
-//    public static boolean isInitialized=initConnection;
-
-    public static void init(Context context, String appid, String Token, AsyncInitCallback callback) {
+    public static void initApp(Context context, String appid, String token, AsyncInitCallback callback) throws UnsupportedEncodingException {
+//        RequestQueue requestQueue = Volley.newRequestQueue(context);
+//        String s = URLEncoder.encode(content, "utf-8");
         NestechFault f = new NestechFault();
         NestechInit ninit = new NestechInit();
-        preferences  = PreferenceManager.getDefaultSharedPreferences(context);
-        if(!initConnection){
-            mSocket.off("init");
-            mSocket.on("init", new Emitter.Listener() {
-                @Override
-                public void call(final Object... args) {
-                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                JSONObject data = new JSONObject(args[0].toString());
-                                boolean ok = data.getBoolean("ok");
-                                String msg = data.getString("msg");
-                                if (ok) {
-                                    SharedPreferences.Editor editor = preferences.edit();
-                                    editor.putString("NesTechAppid", appid);
-                                    editor.putString("NesTechToken", Token);
-                                    editor.apply();
-                                    ninit.setMessage(msg);
-                                    callback.handleResponse(ninit);
-                                } else {
-                                    callback.handleFault(f);
-//                                    isInitialized = false;
-                                }
-                            } catch (JSONException e) {
-//                                isInitialized = false;
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+
+        new Nestech(context);
+        String url = "http://ne20.ir/initApp?appid="+appid+"&token="+token;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //do dome thing
+                try {
+                    JSONObject data = new JSONObject(response);
+                    boolean ok = data.getBoolean("ok");
+                    String msg = data.getString("msg");
+                    if (ok) {
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("NesTechAppid", appid);
+                        editor.putString("NesTechToken", token);
+                        editor.apply();
+                        ninit.setMessage(msg);
+                        callback.handleResponse(ninit);
+                    } else {
+                        f.setMessage(msg);
+                        callback.handleFault(f);
+                    }
+                } catch (JSONException e) {
+                    f.setMessage("Error in connect");
+                    callback.handleFault(f);
+                    e.printStackTrace();
                 }
-            });
-            initConnection=true;
-        }
-
-        mSocket.connect();
-        String jsonStr = "{\"type\":\"init\", \"appid\":\"" + appid + "\", \"Token\":\"" + Token + "\"}";
-        mSocket.emit("init", jsonStr);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //do dome thing
+                f.setMessage("Error in connect");
+                callback.handleFault(f);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> Data = new HashMap<>();
+                Data.put("appid", appid);
+                Data.put("token", token);
+                Data.put("initApp", "initApp");
+                return Data;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
+
     public static class UserService {
-        public static void register( NestechUser user, AsyncCallback callback) {
-//            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-            if(!registerConnection){
-                mSocket.off("register");
-                mSocket.on("register", new Emitter.Listener() {
-                    @Override
-                    public void call(final Object... args) {
-                        new Handler(Looper.getMainLooper()).post(new Runnable() {
-                            @Override
-                            public void run() {
-                                JSONObject data = null;
-                                try {
-                                    data = new JSONObject(args[0].toString());
-                                    boolean ok = data.getBoolean("ok");
-                                    String msg = data.getString("msg");
-                                    if (ok) {
-                                        callback.handleResponse(user);
-                                    } else {
-                                        NestechFault f = new NestechFault();
-                                        f.setMessage(msg);
-                                        callback.handleFault(f);
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                    }
-                });
-                registerConnection=true;
-            }
 
-            mSocket.connect();
-            Map<String, Object> map = new HashMap<String, Object>();
-            map = user.getProperties();
-            if(initConnection && !preferences.getString("NesTechAppid", "").equals("")){
-                map.put("appid", preferences.getString("NesTechAppid", ""));
-                map.put("token", preferences.getString("NesTechToken", ""));
-            }
-            mSocket.emit("register", mapToJson(map));
-        }
-
-        public static void login(NestechUser user, AsyncCallback callback, boolean stayLoggedIn) {
-//                Activity ac =((Activity)context);
-//            Activity ac=(Activity)context;
+        public static void register(  NestechUser user, AsyncCallback callback) throws UnsupportedEncodingException {
             NestechFault f = new NestechFault();
-//            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-            if(!loginConnection){
-                mSocket.off("login");
-                mSocket.on("login", new Emitter.Listener() {
+            if(preferences!=null){
+                Map<String, Object> map = new HashMap<String, Object>();
+                map = user.getProperties();
+                if(!preferences.getString("NesTechAppid", "").equals("")){
+                    map.put("appid", preferences.getString("NesTechAppid", ""));
+                    map.put("token", preferences.getString("NesTechToken", ""));
+                }
+                String url = "http://ne20.ir/register"+mapToQuery(map);
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
                     @Override
-                    public void call(final Object... args) {
-                        new Handler(Looper.getMainLooper()).post(new Runnable() {
-                            @Override
-                            public void run() {
-                                JSONObject data = null;
-                                try {
-                                    data = new JSONObject(args[0].toString());
-                                    boolean ok = data.getBoolean("ok");
-                                    String msg = data.getString("msg");
-
-                                    if (ok) {
-                                        String created_at = data.getString("created_at");
-                                        user.setProperty("created_at",created_at);
-                                        callback.handleResponse(user);
-                                    } else {
-//                                        user.clearProperties();
-
-                                        f.setMessage(msg);
-                                        callback.handleFault(f);
-
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
+                    public void onResponse(String response) {
+                        //do dome thing
+                        try {
+                            JSONObject data = new JSONObject(response);
+                            boolean ok = data.getBoolean("ok");
+                            String msg = data.getString("msg");
+                            if (ok) {
+                                callback.handleResponse(user);
+                            } else {
+                                f.setMessage(msg);
+                                callback.handleFault(f);
                             }
-                        });
+                        } catch (JSONException e) {
+                            f.setMessage(e.getMessage());
+                            callback.handleFault(f);
+                            e.printStackTrace();
+                        }
                     }
-                });
-                loginConnection=true;
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //do dome thing
+                        f.setMessage("Error in connect");
+                        callback.handleFault(f);
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> Data = new HashMap<>();
+                        return Data;
+                    }
+                };
+                requestQueue.add(stringRequest);
             }
-
-            mSocket.connect();
-            Map<String, Object> map = new HashMap<String, Object>();
-            map = user.getProperties();
-            if(initConnection && !preferences.getString("NesTechAppid", "").equals("")) {
-                map.put("appid", preferences.getString("NesTechAppid", ""));
-                map.put("token", preferences.getString("NesTechToken", ""));
+            else{
+                f.setMessage("First call the initApp method");
+                callback.handleFault(f);
             }
-            mSocket.emit("login", mapToJson(map));
         }
+        public static void login(  NestechUser user, AsyncCallback callback) throws UnsupportedEncodingException {
+            NestechFault f = new NestechFault();
+            if(preferences!=null){
+                Map<String, Object> map = new HashMap<String, Object>();
+                map = user.getProperties();
+                if(!preferences.getString("NesTechAppid", "").equals("")){
+                    map.put("appid", preferences.getString("NesTechAppid", ""));
+                    map.put("token", preferences.getString("NesTechToken", ""));
+                }
+                String url = "http://ne20.ir/login"+mapToQuery(map);
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //do dome thing
+                        try {
+                            JSONObject data = new JSONObject(response);
+                            boolean ok = data.getBoolean("ok");
+                            String msg = data.getString("msg");
+                            if (ok) {
+                                String created_at = data.getString("created_at");
+                                user.setProperty("created_at",created_at);
+                                callback.handleResponse(user);
+                            } else {
+                                f.setMessage(msg);
+                                callback.handleFault(f);
+                            }
+                        } catch (JSONException e) {
+                            f.setMessage(e.getMessage());
+                            callback.handleFault(f);
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //do dome thing
+                        f.setMessage("Error in connect");
+                        callback.handleFault(f);
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> Data = new HashMap<>();
+                        return Data;
+                    }
+                };
+                requestQueue.add(stringRequest);
+            }
+            else{
+                f.setMessage("First call the initApp method");
+                callback.handleFault(f);
+            }
+        }
+
 
         static StringBuilder mapToJson(Map<?, ?> data) {
             JSONObject object = new JSONObject();
@@ -203,6 +236,34 @@ public class Nestech {
                 }
             }
             obj.append("}");
+            return obj;
+        }
+        static StringBuilder mapToQuery(Map<?, ?> data) {
+            JSONObject object = new JSONObject();
+            StringBuilder obj = new StringBuilder();
+            int i = 0;
+            for (Map.Entry<?, ?> entry : data.entrySet()) {
+                /*
+                 * Deviate from the original by checking that keys are non-null and
+                 * of the proper type. (We still defer validating the values).
+                 */
+                String key = (String) entry.getKey();
+                if (key == null) {
+                    throw new NullPointerException("key == null");
+                }
+                try {
+                    object.put(key, wrap(entry.getValue()));
+                    if(i==0){
+                        obj.append("?").append(key).append("=").append(entry.getValue());
+                    }
+                    else{
+                        obj.append("&").append(key).append("=").append(entry.getValue());
+                    }
+                    i++;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
             return obj;
         }
     }
